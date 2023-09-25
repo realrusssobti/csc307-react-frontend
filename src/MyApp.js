1c5fbdae0082ceacca9f3594e5fdf654dd2be1af
Russ Sobti <russ@recombinantsoftware.com>
2023-09-25 09:25:42 -0700

    Add State!

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,33 +1,27 @@
-// src/MyApp.js
-import React from "react";
-import Table from "./Table";
 
-// src/MyApp.js
-const characters = [
-    {
-        name: "Charlie",
-        job: "Janitor",
-    },
-    {
-        name: "Mac",
-        job: "Bouncer",
-    },
-    {
-        name: "Dee",
-        job: "Aspring actress",
-    },
-    {
-        name: "Dennis",
-        job: "Bartender",
-    },
-];
+import React, {useState} from 'react';
+import Table from './Table';
 
 function MyApp() {
+    const [characters, setCharacters] = useState([
+        {
+            name: 'Charlie',
+            job: 'Janitor',
+            // the rest of the data
+        },
+    ]);
+
+    function removeOneCharacter (index) {
+        const updated = characters.filter((character, i) => {
+            return i !== index
+        });
+        setCharacters(updated);
+    }
     return (
         <div className="container">
-            <Table characterData={characters} />
+            <Table characterData={characters}
+                   removeCharacter={removeOneCharacter} />
         </div>
-    );
+    )
 }
-
 export default MyApp;
--- src/Table.js
+++ src/Table.js
@@ -18,6 +18,12 @@ function TableBody(props) {
                 <tr key={index}>
                     <td>{row.name}</td>
                     <td>{row.job}</td>
+                    <td>
+                        <button onClick={() =>
+                            props.removeCharacter(index)}>
+                            Delete
+                        </button>
+                    </td>
                 </tr>
             );
         }
@@ -32,7 +38,9 @@ function Table(props) {
     return (
         <table>
             <TableHeader />
-            <TableBody characterData={props.characterData} />
+            <TableBody characterData={props.characterData}
+            removeCharacter={props.removeCharacter}
+            />
         </table>
     );
 }
